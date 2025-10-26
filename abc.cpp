1b495cd1d3451dab10338b611f2e7ed69c8ec32b
Rinza 129 <s2312276129@ru.ac.bd>
2025-10-26 18:34:03 +0600

    modified abc.cpp file

---
+++ abc.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+     int n;
+     cin>>n;
+  
+     for(int i=0;i<n;i++){
+          cout<<"Rinza Faria"<<endl;
+          cout<<"2312276129"<<endl;
+     }
+
+}
